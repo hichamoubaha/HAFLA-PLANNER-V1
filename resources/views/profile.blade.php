fab0b8b7f409a5a51a25b4bfab0c48d464ba2a8d
hichamoubaha <oubahahicham7@gmail.com>
2025-04-17 09:58:51 +0000

    modify the style of side bar

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Settings - User Profile</title>
   <style>
-    /* Reset and base styles */
+    
     * {
       margin: 0;
       padding: 0;
