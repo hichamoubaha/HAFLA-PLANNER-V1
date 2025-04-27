834a3b65a18835e5c552cb2ad5b16c0055e63ace
hichamoubaha <oubahahicham7@gmail.com>
2025-04-27 23:21:02 +0000

    create the review model

---
+++ app/Models/Review.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'service_provider_id',
+        'rating',
+        'comment',
+        'booking_id'
+    ];
+
+    protected $casts = [
+        'rating' => 'integer'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function serviceProvider()
+    {
+        return $this->belongsTo(ServiceProvider::class);
+    }
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+} 
