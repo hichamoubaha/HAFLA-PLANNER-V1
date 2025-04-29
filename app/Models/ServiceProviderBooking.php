fa7c78ec1c1396da35eb11987b71074c23e16427
hichamoubaha <oubahahicham7@gmail.com>
2025-04-29 09:56:58 +0000

    create the ServiceProviderBooking mode

---
+++ app/Models/ServiceProviderBooking.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceProviderBooking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'service_provider_id',
+        'booking_date',
+        'status',
+        'notes'
+    ];
+
+    protected $casts = [
+        'booking_date' => 'datetime'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function serviceProvider()
+    {
+        return $this->belongsTo(ServiceProvider::class);
+    }
+} 
